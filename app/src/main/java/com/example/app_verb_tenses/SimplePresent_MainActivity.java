7d3bb7117781bc44424f741a75fb1b217192a9ab
lorenzozardo <lorenzozardo19@gmail.com>
2023-10-02 09:44:41 -0300

    .

---
+++ app/src/main/java/com/example/app_verb_tenses/SimplePresent_MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_verb_tenses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimplePresent_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_present_main);
+    }
+}
