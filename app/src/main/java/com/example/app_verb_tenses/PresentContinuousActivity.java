6c4e06f584fe8ab4f557245d188eb02727bfe455
Gustavo Costa <79706058+Gustavo-Costa-Code@users.noreply.github.com>
2023-10-09 10:07:24 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/app_verb_tenses/PresentContinuousActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_verb_tenses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PresentContinuousActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_present_continuous);
+    }
+}
