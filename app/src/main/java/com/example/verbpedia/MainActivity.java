bab8aed6227cb25c433524b7826db12d28b8bab9
nathantavaress <nathantavaresr@gmail.com>
2023-11-17 12:18:21 -0300

    17/11

--- app/src/main/java/com/example/verbpedia/MainActivity.java
+++ app/src/main/java/com/example/verbpedia/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.verbpedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnPresentContinuous;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPresentContinuous = findViewById(R.id.btnPresentContinuous);
+        btnPresentContinuous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this, PresentContinuous.class);
+                startActivity(it);
+            }
+        });
+
+        }
+
     }
-}
