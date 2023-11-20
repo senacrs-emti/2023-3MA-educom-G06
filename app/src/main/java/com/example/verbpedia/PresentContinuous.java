ccf69f8db4a5437ae5e24d214f9dc28f8c2f2419
nathantavaress <nathantavaresr@gmail.com>
2023-11-20 09:24:51 -0300

    Update PresentContinuous.java

--- app/src/main/java/com/example/verbpedia/PresentContinuous.java
+++ app/src/main/java/com/example/verbpedia/PresentContinuous.java
@@ -1,32 +1,14 @@
 package com.example.verbpedia;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 public class PresentContinuous extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setTitle("Verbpedia");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_present_continuous);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-    switch (item.getItemId()){
-        case android.R.id.home:
-            startActivity(new Intent(this,PresentContinuous.class));
-            finishAffinity();
-            break;
-        default:break;
-    }
-    return true;
     }
 }
